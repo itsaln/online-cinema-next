55576e410f5a49ece048e9d37169b5e253912357
itsaln <itsaln23@gmail.com>
2022-11-13 20:22:55 +0500

    auth

--- app/components/layout/Navigation/MenuContainer/auth/AuthItems.tsx
+++ app/components/layout/Navigation/MenuContainer/auth/AuthItems.tsx
@@ -1,8 +1,47 @@
 import { FC } from 'react'
 
+import MenuItem from '@/components/layout/Navigation/MenuContainer/MenuItem'
+import LogoutButton from '@/components/layout/Navigation/MenuContainer/auth/LogoutButton'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import { getAdminHomeUrl } from '@/configs/url.config'
+
 const AuthItems: FC = () => {
+	const { user } = useAuth()
+
 	return (
-	    <div>AuthItems</div>
+		<>
+			{user ? (
+				<>
+					<MenuItem
+						item={{
+							icon: 'MdSettings',
+							link: '/profile',
+							title: 'Profile'
+						}}
+					/>
+					<LogoutButton />
+				</>
+			) : (
+				<MenuItem
+					item={{
+						icon: 'MdLogin',
+						link: '/auth',
+						title: 'Login'
+					}}
+				/>
+			)}
+			{user?.isAdmin && (
+				<MenuItem
+					item={{
+						icon: 'MdOutlineLock',
+						link: getAdminHomeUrl(),
+						title: 'Admin panel'
+					}}
+				/>
+			)}
+		</>
 	)
 }
 
--- app/components/layout/Navigation/MenuContainer/auth/LogoutButton.tsx
+++ app/components/layout/Navigation/MenuContainer/auth/LogoutButton.tsx
@@ -1,8 +1,23 @@
-import { FC } from 'react'
+import { FC, MouseEvent } from 'react'
+
+import MaterialIcon from '@/ui/MaterialIcon'
+
+import { useActions } from '@/hooks/useActions'
 
 const LogoutButton: FC = () => {
+	const { logout } = useActions()
+	const handleLogout = (e: MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault()
+		logout()
+	}
+
 	return (
-	    <div>LogoutButton</div>
+		<li>
+			<a onClick={handleLogout}>
+				<MaterialIcon name='MdLogout' />
+				<span>Logout</span>
+			</a>
+		</li>
 	)
 }
 
