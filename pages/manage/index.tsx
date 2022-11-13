93073564d227eb2bd50f3c0709223ff896ce2c14
itsaln <itsaln23@gmail.com>
2022-11-13 21:39:08 +0500

    auth

---
+++ pages/manage/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const AdminPage: NextPage = () => {
+	return (
+	    <div>Admin</div>
+	)
+}
+
+export default AdminPage
---
+++ pages/profile.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const ProfilePage: NextPage = () => {
+	return (
+	    <div>Profile</div>
+	)
+}
+
+export default ProfilePage
