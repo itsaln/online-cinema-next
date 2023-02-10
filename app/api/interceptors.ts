4b9ba51ee60b084bc7c93c8edef326a1c63e9e96
itsaln <itsaln23@gmail.com>
2023-02-10 10:42:25 +0500

    fix

--- app/api/interceptors.ts
+++ app/api/interceptors.ts
@@ -15,7 +15,7 @@ export const axiosClassic = axios.create({
 })
 
 export const instance = axios.create({
-	baseURL: IS_PRODUCTION ? API_SERVER_URL : API_URL,
+	baseURL: API_SERVER_URL,
 	headers: getContentType()
 })
 
