7743a2d5b8b2d2b94fbdd734b1905c51fc44d180
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-12-10 18:48:38 +0500

    sss

--- next.config.js
+++ next.config.js
@@ -13,13 +13,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				// destination: 'http://localhost:5000/api/:path*'
-				destination: 'https://online-cinema-api-3tf2.onrender.com/api/:path*'
+				destination: `${process.env.REACT_APP_SERVER_URL}/api/:path*`
 			},
 			{
 				source: '/uploads/:path*',
-				// destination: 'http://localhost:5000/uploads/:path*'
-				destination: 'https://online-cinema-api-3tf2.onrender.com/uploads/:path*'
+				destination: `${process.env.REACT_APP_SERVER_URL}/uploads/:path*`
 			}
 		]
 	}
