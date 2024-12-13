1722c9f86a254f9431264b882c79db5460bff5b1
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-12-13 11:06:09 +0500

    env

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: `${process.env.REACT_APP_SERVER_URL}/api/:path*`
+				destination: `${process.env.APP_SERVER_URL}/api/:path*`
 			},
 			{
 				source: '/uploads/:path*',
-				destination: `${process.env.REACT_APP_SERVER_URL}/uploads/:path*`
+				destination: `${process.env.APP_SERVER_URL}/uploads/:path*`
 			}
 		]
 	}
