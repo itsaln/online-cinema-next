a0dd8e5308d764f02b3d91a58fcfe1b49af7c0d8
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-12-13 11:01:43 +0500

    env

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
 	swcMinify: true,
 	poweredByHeader: false,
 	optimizeFonts: false,
-	env: {
-		APP_URL: process.env.REACT_APP_URL,
-		APP_ENV: process.env.REACT_APP_ENV,
-		APP_SERVER_URL: process.env.REACT_APP_SERVER_URL
-	},
 	async rewrites() {
 		return [
 			{
